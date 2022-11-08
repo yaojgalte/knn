3cd9443017a7dc55847c08410b73793f84f06b57
yaoj90 <sherryyaoj@hotmail.com>
2022-11-08 14:41:25 +0100

    add

--- app.py
+++ app.py
@@ -1,70 +1,28 @@
 import streamlit as st
-from sklearn.datasets import load_wine, load_breast_cancer, load_iris
-from  sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-def getClassifier(classifier):
-    if classifier == 'SVM':
-        c = st.sidebar.slider(label='Choose value of C' , min_value=0.0001, max_value=10.0)
-        model = SVC(C=c)
-    elif classifier == 'KNN':
-        neighbors = st.sidebar.slider(label='Choose Number of Neighbors',min_value=1,max_value=20)
-        model = KNeighborsClassifier(n_neighbors = neighbors)
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 10)
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        model = RandomForestClassifier(max_depth = max_depth , n_estimators= n_estimators,random_state= 1)
-    return model
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+#df = pd.read_csv("diabetes.csv")
+
+# title
+st.title("Diabetes predictor")
+st.subheader("This model will predict if a person would have diabetes or not. "
+             "If the intput value is missing, please enter 0.")
+
+Pregnancies = st.text_input("Gravidity (the number of times that a woman has been pregnant).")
+Glucose = st.text_input("Glucose")
+BloodPressure = st.text_input("BloodPressure")
+SkinThickness = st.text_input("SkinThickness")
+Insulin = st.text_input("Insulin")
+BMI = st.text_input("BMI")
+DiabetesPedigreeFunction = st.text_input("DiabetesPedigreeFunction")
+Age = st.text_input("Age")
+
+input = pd.Series([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+pd.to_numeric(input) # convert everything to float values
 
 
-def getPCA(df):
-    pca = PCA(n_components=3)
-    result = pca.fit_transform(df.loc[:,df.columns != 'Type'])
-    df['pca-1'] = result[:, 0]
-    df['pca-2'] = result[:, 1]
-    df['pca-3'] = result[:, 2]
-    return df
-def return_data(dataset):
-    if dataset == 'Wine':
-        data = load_wine()
-    elif dataset == 'Iris':
-        data = load_iris()
-    else:
-        data = load_breast_cancer()
-    df = pd.DataFrame(data.data, columns=data.feature_names , index=None)
-    df['Type'] = data.target
-    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, random_state=1, test_size=0.2)
-    return X_train, X_test, y_train, y_test,df,data.target_names
-
-# Title
-st.title("Title")
-
-# Description
-st.text("description")
-
-#sidebar
-sideBar = st.sidebar
-dataset = sideBar.selectbox('options?',('A' , 'B' , 'C'))
-
-# info
-classifier = 'Random Forest'
-
-X_train, X_test, y_train, y_test, df , classes= return_data(dataset)
-st.dataframe(df.sample(n = 5 , random_state = 1))
-st.subheader("Classes")
-
-
-# Train Model
-# Train Model
-model = getClassifier(classifier)
-model.fit(X_train, y_train)
-test_score = round(model.score(X_test, y_test), 2)
-train_score = round(model.score(X_train, y_train), 2)
-
-st.subheader('Train Score: {}'.format(train_score))
-st.subheader('Test Score: {}'.format(test_score))
