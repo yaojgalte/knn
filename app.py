818646ab79a00f3901208983cef50ce4093d083a
yaoj90 <sherryyaoj@hotmail.com>
2022-11-08 14:56:21 +0100

    add

--- app.py
+++ app.py
@@ -13,17 +13,16 @@ st.title("Diabetes predictor")
 st.subheader("This model will predict if a person would have diabetes or not. "
              "If the intput value is missing, please enter 0.")
 
-Pregnancies = st.text_input("Gravidity (the number of times that a woman has been pregnant).")
-Glucose = st.text_input("Glucose")
-BloodPressure = st.text_input("BloodPressure")
-SkinThickness = st.text_input("SkinThickness")
-Insulin = st.text_input("Insulin")
-BMI = st.text_input("BMI")
-DiabetesPedigreeFunction = st.text_input("DiabetesPedigreeFunction")
-Age = st.text_input("Age")
+Pregnancies = int(st.text_input("Gravidity (the number of times that a woman has been pregnant)."))
+Glucose = int(st.text_input("Glucose"))
+BloodPressure = int(st.text_input("BloodPressure"))
+SkinThickness = int(st.text_input("SkinThickness"))
+Insulin = int(st.text_input("Insulin"))
+BMI = float(st.text_input("BMI"))
+DiabetesPedigreeFunction = float(st.text_input("DiabetesPedigreeFunction"))
+Age = int(st.text_input("Age"))
 
-input = pd.Series([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-pd.to_numeric(input) # convert everything to float values
+input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
 
 # knn imputation strategy and
 # prediction for the diabetes
